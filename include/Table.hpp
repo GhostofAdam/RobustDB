118e30fc675eeace8a615761d9f1f198dd8eb294
桂尚彤 <gst17@mails.tsinghua.edu.cn>
2020-10-26 21:49:33 +0800

    ~

--- include/Table.hpp
+++ include/Table.hpp
@@ -1,5 +1,24 @@
 #include "common.hpp"
+#include "setting.hpp"
+
+struct TableHead {
+    int8_t columnTot, primaryCount, checkTot, foreignKeyTot;
+    int pageTot, recordByte, dataArrUsed;
+    unsigned int nextAvail, notNull, hasIndex, isPrimary;
+};
 
 class Table{
-
+public:
+    void allocPage();
+    RID_t getNext(RID_t rid);
+    void getRecord(RID_t rid, char *buf);
+    void dropRecord(RID_t rid);
+    int getRecordBytes();
+private:
+    TableHead head_;
+    string name_;
+    void create(string tableName);
+    void open(string tableName);
+    void close();
+    void drop();
 };
