ee4a13deee73e69eb130474827dfda15ce5c4ce2
Lucas <jiangwy17@163.com>
2020-11-16 15:39:50 +0800

    Index

---
+++ include/RegisterManager.hpp
@@ -0,0 +1,30 @@
+#include "common.hpp"
+
+class Table;
+
+class RegisterManager {
+private:
+    map<int, Table*> list;
+    RegisterManager() = default;
+    ~RegisterManager() = default;
+
+public:
+    RegisterManager(RegisterManager const &) = delete;
+    RegisterManager &operator=(RegisterManager const &) = delete;
+    static RegisterManager &getInstance() {
+        static RegisterManager instance;
+        return instance;
+    }
+    void checkIn(int permID, Table *table) {
+        assert(list.find(permID) == list.end());
+        list[permID] = table;
+    }
+    void checkOut(int permID) {
+        assert(list.count(permID) > 0);
+        list.erase(permID);
+    }
+    Table *getPtr(int permID) {
+        assert(list.find(permID) == list.end());
+        return list[permID];
+    }
+};
--- include/common.hpp
+++ include/common.hpp
@@ -2,17 +2,22 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <queue>
 #include <math.h>
 #include <cmath>
 #include <assert.h>
+#include <map>
 
 using std::cout;
 using std::endl;
 using std::cin;
 using std::vector;
 using std::string;
+using std::ostringstream;
 using std::ifstream;
+using std::ofstream;
 using std::ios;
 using std::queue;
 using std::ceil;
+using std::map;
