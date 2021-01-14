c018bb874148251e41d28652ca678177753096fe
Lucas <jiangwy17@163.com>
2021-01-15 01:30:43 +0800

    init and insert

--- test/init.py
+++ test/init.py
@@ -2,6 +2,15 @@ import os
 import sys
 import pandas as pd
 
+mapping = {"part": [False, True, True, True, True, False, True, False, True],
+           "region": [False, True, True],
+           "nation": [False, True, False, True],
+           "supplier": [False, True, True, False, True, False, True],
+           "customer": [False, True, True, False, True, False, True, True],
+           "partsupp": [False, False, False, False, True],
+           "orders": [False, False, True, False, True, True, True, False, True],
+           "lineitem": [False, False, False, False, False, False, False, False, True, True, True, True, True, True, True, True]}
+
 def main():
     dir = "./dataset_test"
     fr = open("./init.sql", "w")
@@ -10,6 +19,7 @@ def main():
     fr.write("\n\n")
     for filename in list(os.listdir(dir)):
         table_name = filename.split(".")[0]
+        table_map = mapping[table_name]
         with open(os.path.join(dir, filename), "r", encoding="utf-8") as f:
             lines = f.readlines()
         if filename.endswith(".csv"):
@@ -23,7 +33,7 @@ def main():
             line = line.strip().split("|")
             if filename.endswith(".tbl"):
                 line = line[:-1]
-            fr.write("INSERT INTO {} VALUES ".format(table_name) + "(" + ",".join(["\'{}\'".format(x) for x in line]) + ");\n")
+            fr.write("INSERT INTO {} VALUES ".format(table_name) + "(" + ",".join(["\'{}\'".format(x) if y else x for x, y in zip(line, table_map)]) + ");\n")
         fr.write("\n\n")
     fr.close()
 
