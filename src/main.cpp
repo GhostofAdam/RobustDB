327c009dcef25f1657a4dcb5bf8446a50d74d94d
GhostofAdam <gst0601@163.com>
2021-01-16 15:41:20 +0800

    ??

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ extern "C" char start_parse(const char *expr_input);
 #endif
 bool noCheck = false;
 int main(int argc, char const *argv[]) {
-    if (argc == 3 && strcmp("noCheck", argv[2]) == 0){
+    if (argc == 2 && strcmp("noCheck", argv[1]) == 0){
         noCheck = true;
     }
     printf("Starting RobustDB...\n");
